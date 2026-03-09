28236b0d4677a9d2ccac924d1e225a0ce201ff49
veroniquebvs <beauvais.veronique@gmail.com>
2026-03-09 17:02:42 +0100

    include password hashing

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,14 +1,20 @@
 from flask import Flask
 from flask_restx import Api
-from app.api.v1.users import api as users_ns
-from app.api.v1.amenities import api as amenities_ns
-from app.api.v1.places import api as places_ns
-from app.api.v1.reviews import api as reviews_ns
+from flask_bcrypt import Bcrypt
+from config import DevelopmentConfig
+
+bcrypt = Bcrypt()
 
 
-def create_app(config_class="config.DevelopmentConfig"):
+def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    bcrypt.init_app(app)
+
+    from app.api.v1.users import api as users_ns
+    from app.api.v1.amenities import api as amenities_ns
+    from app.api.v1.places import api as places_ns
+    from app.api.v1.reviews import api as reviews_ns
 
     api = Api(
         app,
--- part3/app/api/v1/users.py
+++ part3/app/api/v1/users.py
@@ -5,10 +5,22 @@ api = Namespace('users', description='User operations')
 
 # Define the user model for input validation and documentation
 user_model = api.model('User', {
-    'first_name': fields.String(required=True, description='First name of the user'),
-    'last_name': fields.String(required=True, description='Last name of the user'),
-    'email': fields.String(required=True, description='Email of the user'),
-    'password': fields.String(required=True, description='Password of the user')
+    'first_name': fields.String(
+        required=True,
+        description='First name of the user'
+    ),
+    'last_name': fields.String(
+        required=True,
+        description='Last name of the user'
+    ),
+    'email': fields.String(
+        required=True,
+        description='Email of the user'
+    ),
+    'password': fields.String(
+        required=True,
+        description='Password of the user'
+    )
 })
 
 
