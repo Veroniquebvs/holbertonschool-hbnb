eb3e00b81d811d8dd39b1cfe6db9904d6b607068
veroniquebvs <beauvais.veronique@gmail.com>
2026-03-12 14:18:22 +0100

    add sqlalchemy

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -3,10 +3,12 @@ from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from config import DevelopmentConfig
+from flask_sqlalchemy import SQLAlchemy
 
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
+db = SQLAlchemy()
 
 
 def create_app(config_class=DevelopmentConfig):
@@ -15,6 +17,7 @@ def create_app(config_class=DevelopmentConfig):
     bcrypt.init_app(app)
     app.config["JWT_SECRET_KEY"] = "SECRET_KEY"
     jwt.init_app(app)
+    db.init_app(app)
 
     from app.api.v1.users import api as users_ns
     from app.api.v1.amenities import api as amenities_ns
